6c58ac662ba6512b94d3e3484e9edb93aa5479fa
= <=>
2016-05-19 20:57:51 -0700

    Initial commit, add base controls + BUILD

---
+++ state_space.h
@@ -0,0 +1,18 @@
+#include "eigen/Eigen/Core"
+
+template <int NumStates, int NumInputs, int NumSensors>
+class StateSpace {
+public:
+  void SetGoal(Eigen::Matrix <double, NumStates, 1> R );
+  void SetSystem(Eigen::Matrix <double, NumStates, NumStates> A,
+                 Eigen::Matrix <double, NumStates, NumInputs> B,
+                 Eigen::Matrix <double, NumSensors, NumStates> C);
+protected:
+  Eigen::Matrix <double, NumStates, 1> X_;
+  Eigen::Matrix <double, NumStates, 1> R_;
+  Eigen::Matrix <double, NumSensors, 1> Y_;
+  Eigen::Matrix <double, NumInputs, 1> U_;
+  Eigen::Matrix <double, NumStates, NumStates> A_;
+  Eigen::Matrix <double, NumStates, NumInputs> B_;
+  Eigen::Matrix <double, NumSensors, NumStates> C_;
+}
